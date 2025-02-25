b2a803e8ba4bc9fe2758e0e081e653bb1fb4e34e
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2025-02-25 23:04:44 +0530

    Add new gemini model

--- app/api/convert/route.ts
+++ app/api/convert/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request) {
 
     const { text } = await req.json();
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
     const prompt = `DO NOT PARAPHRASE OR MODIFY THE CONTENT IN ANY WAY! Your task is ONLY to convert the text to markdown format.
 
